e3e239dee4c8bc85ebc74ad984c0d258b7164afb
João Paulo Azevedo <89151731+jpazv@users.noreply.github.com>
2021-10-13 16:48:43 -0300

    Add files via upload

---
+++ progressões/soma-PA.js
@@ -0,0 +1,29 @@
+function somaPA(inicio, razao, termos) {
+    if(termos>0){    
+        if(razao<0) {
+            let somatorio = inicio
+            let array_termos = []
+            let reducer = (previousValue, currentValue) => previousValue + currentValue;
+            for(let i = inicio; i >= (inicio + (termos-1)*razao); i+=razao) {
+                array_termos.push(somatorio)
+                somatorio += razao;
+            }
+            return console.log(array_termos.reduce(reducer));
+    }
+    else{
+        let somatorio = inicio
+        let array_termos = []
+        let reducer = (previousValue, currentValue) => previousValue + currentValue;
+        for(let i = inicio; i <= (inicio + (termos-1)*razao); i+=razao) {
+            array_termos.push(somatorio)
+            somatorio+= razao;
+        }
+        return console.log(array_termos.reduce(reducer));
+        }
+    }
+    else{
+        console.log('É necessário haver pelo menos um termo')
+    }    
+}
+somaPA(1,-1, 10)
+
