aa49820cf559a8e1deb9afbcb10d8541bd4eef1f
João Paulo Azevedo <89151731+jpazv@users.noreply.github.com>
2021-10-13 15:18:13 -0300

    Update termos-pa.js

--- progressões/termos-pa.js
+++ progressões/termos-pa.js
@@ -1,15 +1,17 @@
 function PA(inicio, razao, termos) {
     if(termos>0){    
         if(razao<0) {
+            let somatorio = inicio
             for(let i = inicio; i >= (inicio + (termos-1)*razao); i+=razao) {
-                console.log(i);
-                i += r;
+                console.log(somatorio);
+                somatorio += razao;
             }
         }
         else{
+            let somatorio = inicio
             for(let i = inicio; i <= (inicio + (termos-1)*razao); i+=razao) {
-                console.log(i);
-               i += r;
+                console.log(somatorio);
+                somatorio+= razao;
             }
         }
     }
