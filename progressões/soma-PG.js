58567cdf9ccf216cb624d239e3ff671aadacc26a
João Paulo Azevedo <89151731+jpazv@users.noreply.github.com>
2021-10-13 17:02:29 -0300

    Update soma-PG.js

--- progressões/soma-PG.js
+++ progressões/soma-PG.js
@@ -1,9 +1,9 @@
-function somaPG(inicio, razao, termos) {
-    let somatorio = inicio;
-    let array_termos = []
-    let reducer = (previousValue, currentValue) => previousValue + currentValue;
-    
+function somaPG(inicio, razao, termos) {    
     if(termos > 1 && razao != 0 && Number.isInteger(termos)){
+        let somatorio = inicio;
+        let array_termos = []
+        let reducer = (previousValue, currentValue) => previousValue + currentValue;
+
         for(let i = inicio; i <= (inicio)*(Math.pow(razao, termos-1)); i *= Math.abs(razao)){
             array_termos.push(somatorio)
             somatorio *= razao;
@@ -14,5 +14,3 @@ function somaPG(inicio, razao, termos) {
         console.log('É necessário haver pelo menos um termo e a razão ser diferente de 0')
     }
 }
-
-console.log(somaPG(1,2,5))
