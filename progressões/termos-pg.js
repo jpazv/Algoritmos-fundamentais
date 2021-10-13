a29b303eed0402b6f487fc3c253a96c9a74b3a82
João Paulo Azevedo <89151731+jpazv@users.noreply.github.com>
2021-10-13 15:58:11 -0300

    Update termos-pg.js

--- progressões/termos-pg.js
+++ progressões/termos-pg.js
@@ -2,12 +2,12 @@ function PG(inicio, razao, termos) {
     let somatorio = inicio;
     
     if(termos > 1 && razao != 0){
-        for(let i = Math.abs(inicio); i <= Math.abs(inicio)*(Math.pow(razao, termos-1)); i *= razao){
+        for(let i = Math.abs(inicio); i <= Math.abs(inicio)*(Math.pow(razao, termos-1)); i *= Math.abs(razao)){
             console.log(somatorio);
             somatorio *= razao;
         }
     }
     else {
-        console.log('Não é possível ter um número negativo de termos')
+        console.log('Não é possível ter um número negativo de termos ou razão igual a zero')
     }
 }
